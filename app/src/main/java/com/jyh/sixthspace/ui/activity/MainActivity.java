2ca858d253730ec21003a9ba1398b123eeddffbc
jyh <847145871@qq.com>
2017-10-09 05:49:12 +0800

    就这样了

--- app/src/main/java/com/jyh/sixthspace/ui/activity/MainActivity.java
+++ app/src/main/java/com/jyh/sixthspace/ui/activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.jyh.sixthspace.R;
 import com.jyh.sixthspace.databinding.ActivityMainBinding;
